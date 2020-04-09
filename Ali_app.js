674e2e811d1f1b67ef08ba3f8dc0831e29062bb9
Ali Ahmad Mansoor <55711063+aliahmad147@users.noreply.github.com>
2020-04-09 14:41:15 +0500

    Task2_

---
+++ Ali_app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Ali_app extends Component {
+render() {
+  return (
+   <div align="center" margin="10%">
+      <h1>Ali Ahmad 16i-0147</h1>
+      <img src={"https://builtin.com/sites/default/files/styles/ckeditor_optimize/public/inline-images/blockchain-revolution-ethereum_0.jpg"} className="App-logo" style = {{ width: 400, height: 300 }} />
+  </div>
+  ); 
+}
+}
+export default Ali_app;
